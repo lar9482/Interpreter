0e48b97e0ca03aa3f406a7d1a18b9dc26b4b40cf
Luke Runnels <lar9482@utulsa.edu>
2023-07-13 22:49:18 -0500

    Setting up outline to begin parsing statements

---
+++ AST/ReturnStmtAST.ts
@@ -0,0 +1,15 @@
+import ExprAST from "./ExprAST";
+import { NodeType } from "./NodeType";
+import StmtAST from "./StmtAST";
+
+export default class ReturnStmtAST extends StmtAST {
+
+    returnValue: ExprAST;
+
+    constructor(type: NodeType, sourceLineNumber: number,
+        returnValue: ExprAST) {
+        super(type, sourceLineNumber);
+
+        this.returnValue = returnValue;
+    }
+}
