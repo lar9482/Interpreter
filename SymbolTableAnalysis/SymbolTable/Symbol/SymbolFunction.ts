cd3ad2faa2dfded01827ef81a5cee0a43ddd25a2
larunnel161 <larunnel161@gmail.com>
2023-08-15 01:44:12 -0500

    Saving a reference to the actual function declaration AST node within a symbol function entry.

--- SymbolTableAnalysis/SymbolTable/Symbol/SymbolFunction.ts
+++ SymbolTableAnalysis/SymbolTable/Symbol/SymbolFunction.ts
@@ -2,15 +2,19 @@ import Symbol from "./Symbol";
 import { SymbolType } from "./SymbolType";
 import { DecafType } from "../../../AST/DecafType";
 import ParameterAST from "../../../AST/ParameterAST";
+import FuncDeclAST from "../../../AST/FuncDeclAST";
 
 export default class SymbolFunction extends Symbol {
 
     parameters: ParameterAST[]
+    functionDeclaration?: FuncDeclAST;
 
     constructor(symbolType: SymbolType, name: string, returnType: DecafType,
-        parameters: ParameterAST[]) {
+        parameters: ParameterAST[],
+        functionDeclaration?: FuncDeclAST) {
 
         super(symbolType, name, returnType);
         this.parameters = parameters;
+        this.functionDeclaration = functionDeclaration;
     }
 }
