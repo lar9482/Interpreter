6375a99614d8714d0716d7edc90c66f42f037d24
larunnel161 <larunnel161@gmail.com>
2023-08-17 17:13:37 -0500

    Annotating ExprASTs with 'value' for interpretation later.

--- AST/ExprAST/IntLiteralAST.ts
+++ AST/ExprAST/IntLiteralAST.ts
@@ -3,12 +3,11 @@ import { DecafType } from "../DecafType";
 import LiteralAST from "./LiteralAST";
 
 export default class IntLiteralAST extends LiteralAST {
-    int: number;
 
     constructor(type: NodeType, sourceLineNumber: number,
         literalType: DecafType, int: number) {
 
         super(type, sourceLineNumber, literalType);
-        this.int = int;
+        this.value = int;
     }
 }
