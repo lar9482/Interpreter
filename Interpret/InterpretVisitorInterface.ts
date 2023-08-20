3c94f08afb7de9a2f16cc9584d320051e8a88b6d
lar9482 <lar9482@utulsa.edu>
2023-08-19 21:34:35 -0500

    Implementation interpretation for assignments

--- Interpret/InterpretVisitorInterface.ts
+++ Interpret/InterpretVisitorInterface.ts
@@ -7,12 +7,14 @@ import BinaryExprAST from "../AST/ExprAST/BinaryExprAST";
 import ExprAST from "../AST/ExprAST/ExprAST";
 import UnaryExprAST from "../AST/ExprAST/UnaryExprAST";
 import LocAST from "../AST/ExprAST/LocAST";
+import AssignStmtAST from "../AST/StmtAST/AssignStmtAST";
 
 export default interface interpretVisitorInterface {
     //Base nodes for the visitor
     interpretFuncDecl: (funcDeclAST: FuncDeclAST) => void,
     interpretBlock: (blockAST: BlockAST) => void,
 
+    interpretAssignStmtAST: (assignStmtAST: AssignStmtAST) => void,
     interpretReturnStmtAST: (returnStmtAST: ReturnStmtAST) => void,
 
     interpretExpr: (exprAST: ExprAST) => void,
